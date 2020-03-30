eec5c94f852c5b0ccf1e097ea8a02b1a5c9c30c9
Pavas <pavas.sah@gmail.com>
2020-03-30 17:26:09 +0530

    Added Jest Dependency

---
+++ src/__test__/Login-Screen.connected.test.tsx
@@ -0,0 +1,11 @@
+
+import React, {  } from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import LoginScreen from "../component/Login-Screen.connected";
+
+describe( <LoginScreen />, () =>{
+    test("", async () =>{
+        // ???
+    });
+});
